c487152b010776509c600dad4e1d46185adac817
enes-polat <56820380+enes-polat@users.noreply.github.com>
2020-03-06 15:08:06 +0300

    Add project files.

---
+++ WFA_MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace WFA_MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flwContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // flwContainer
+            // 
+            this.flwContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flwContainer.Location = new System.Drawing.Point(20, 60);
+            this.flwContainer.Name = "flwContainer";
+            this.flwContainer.Size = new System.Drawing.Size(684, 393);
+            this.flwContainer.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 473);
+            this.Controls.Add(this.flwContainer);
+            this.Name = "Form1";
+            this.Text = "Mayın Tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flwContainer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WFA_MayinTarlasi/Form1.cs
@@ -0,0 +1,87 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WFA_MayinTarlasi
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int[] mayins = new int[20];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(361, 397);
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                int mayin = rnd.Next(1, 101);
+                if (!mayins.Contains(mayin))
+                    mayins[i] = mayin;
+                else
+                    i--;
+            }
+
+
+            for (int i = 1; i < 101; i++)
+            {
+                PictureBox pcb = new PictureBox();
+                pcb.Width = 30;
+                pcb.Height = 30;
+                pcb.Margin = new Padding(1);
+                pcb.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+
+                if (mayins.Contains(i))
+                {
+                    pcb.Tag = true;
+                }
+
+                pcb.Click += Pcb_Click;
+
+                flwContainer.Controls.Add(pcb);
+            }
+
+            timer1.Start();
+        }
+
+        private void Pcb_Click(object sender, EventArgs e)
+        {
+            PictureBox pcb = (PictureBox)sender;
+            if (pcb.Tag != null)
+            {
+                timer1.Stop();
+                foreach (PictureBox item in flwContainer.Controls)
+                {
+                    if (item.Tag != null)
+                    {
+                        item.BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        item.BackColor = Color.Yellow;
+                    }
+                }
+            }
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            //this.Text = this.Size.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (Control item in flwContainer.Controls)
+            {
+                item.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            }
+        }
+    }
+}
